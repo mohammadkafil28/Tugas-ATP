e0390da787978a7ef641849d86992a010a496522
Mohammad Kafil <98546188+mohammadkafil28@users.noreply.github.com>
2023-07-04 08:45:07 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\ProdukController;
+
+Route::get('/produk', [ProdukController::class, 'index']);
+Route::get('/produk/create', [ProdukController::class, 'create']);
+Route::post('/produk', [ProdukController::class, 'store']);
+Route::get('/produk/{id}', [ProdukController::class, 'show']);
+Route::get('/produk/{id}/edit', [ProdukController::class, 'edit']);
+Route::put('/produk/{id}', [ProdukController::class, 'update']);
+Route::delete('/produk/{id}', [ProdukController::class, 'destroy']);
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
